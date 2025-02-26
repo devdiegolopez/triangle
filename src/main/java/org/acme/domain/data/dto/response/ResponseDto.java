a3f28fd487e85bd4307baa4a927a6fdcb10ec437
Diego <diego.gutierrez@chakray.com>
2025-02-26 13:39:30 -0600

    adicion de responseDto

--- src/main/java/org/acme/domain/data/dto/response/ResponseDto.java
+++ src/main/java/org/acme/domain/data/dto/response/ResponseDto.java
@@ -1,5 +1,24 @@
 package org.acme.domain.data.dto.response;
 
+import org.acme.domain.data.dto.request.RequestDto;
+
 public class ResponseDto {
-    String res;
+    RequestDto requestDto;
+    public ResponseDto(RequestDto requestDto) {
+        this.requestDto = requestDto;
+    }
+    public RequestDto getRequestDto() {
+        return requestDto;
+    }
+
+    public void setRequestDto(RequestDto requestDto) {
+        this.requestDto = requestDto;
+    }
+
+    @Override
+    public String toString() {
+        return "ResponseDto{" +
+                "requestDto=" + requestDto +
+                '}';
+    }
 }
